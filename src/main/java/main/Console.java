a033ab909b96a3f4a4e15a4c7cdc71381bb7082b
Jakub <Blade123164@gmail.com>
2018-09-19 21:39:34 +0200

    init commit

--- src/main/java/main/Console.java
+++ src/main/java/main/Console.java
@@ -1,8 +1,19 @@
 package main;
 
+import java.io.IOException;
+
 public abstract class Console {
 
     public static void clear() {
-        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+        try {
+       if (System.getProperty("os.name").contains("Windows")) {
+           new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+       }else {
+
+               Runtime.getRuntime().exec("clear");
+           }} catch (IOException | InterruptedException e) {
+               e.printStackTrace();
+           }
     }
-}
+      /*  System.out.println(new String(new char[50]).replace("\0", "\r\n"));*/
+    }
